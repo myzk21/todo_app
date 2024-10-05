df3794ef31175993dc8c0d33c2bd1a648af0f103
Sota Miyazaki <luckypaper@icloud.com>
2024-10-06 02:34:40 +0900

    [Update] 入力フォームの作成

--- resources/ts/todo_list/app.ts
+++ resources/ts/todo_list/app.ts
@@ -17,3 +17,26 @@ checkBox.addEventListener('change', () => {
         monthlyGoal.style.display = 'none';
     }
 });
+
+const detailBtn = document.getElementById('detail') as HTMLInputElement;
+const todoContent = document.getElementById('todo_content') as HTMLElement;
+const downArrow = document.getElementById('down_arrow') as HTMLElement;
+const detailSign = document.getElementById('detail_sign') as HTMLElement;
+
+todoContent.classList.add('opacity-0', 'transition-opacity', 'duration-500');
+
+detailBtn.addEventListener('click', () => {
+
+    if (todoContent.style.display === 'block') {
+        todoContent.style.display = 'none';
+        detailSign.textContent = '詳細';
+    } else {
+        todoContent.style.display = 'block';
+        detailSign.textContent = '閉じる';
+    }
+
+    setTimeout(() => {
+        todoContent.classList.toggle('opacity-0');
+    }, 10);
+    downArrow.classList.toggle('custom-rotate-180');//矢印回転
+});
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -9,10 +9,10 @@
                 </div>
             </label>
 
-            <div class="w-3/4 mb-6 bg-white px-6 py-3">{{--shadow-md rounded-lg--}}
+            <div class="w-3/4 mb-6 bg-white px-6 py-3">
                 <div class="flex">
                     <h2 class="text-xl font-bold">Weekly Goal / 週間目標</h2>
-                    <p class="ml-auto cursor-pointer text-gray-400 text-sm flex hover:underline">作成
+                    <p class="ml-auto cursor-pointer text-gray-400 text-sm flex hover:underline select-none">作成
                     </p>
                 </div>
                     <p class="text-gray-500">A list of all the users in your account including their name, title, email and role.</p>
@@ -21,61 +21,59 @@
                     <div class="border border-gray-200 my-4"></div>
                     <div class="flex">
                         <h2 class="text-xl font-bold">Monthly Goal / 月間目標</h2>
-                        <p class="ml-auto cursor-pointer text-gray-400 text-sm flex hover:underline">作成
+                        <p class="ml-auto cursor-pointer text-gray-400 text-sm flex hover:underline select-none">作成
                         </p>
                     </div>
                     <p class="text-gray-500">A list of all the users in your account including their name, title, email and role.</p>
                 </div>
             </div>
 
-            {{-- <div class="mb-6 w-3/4 bg-white px-6 py-3">{{--shadow-md rounded-lg--}}
-                {{-- <div class="flex">
-                    <p class="cursor-pointer text-gray-400 text-sm flex hover:underline flex">詳細入力
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                        </svg>
-                    </p>
-                    <input type="text" class="border border-gray-500 rounded h-8 placeholder:text-sm placeholder:text-gray-300 w-3/4" placeholder="TODOを入力">
-                    <button class="bg-[#8b8a8e] text-white text-sm px-4 py-2 rounded h-8 ml-auto hover:bg-opacity-80">追加</button>
-                </div>
-            </div> --}}
-
-
             <div class="bg-white shadow-md rounded-lg w-3/4 overflow-x-auto">
                 <form action="" method="" class="" id="">
-                    <div class="flex px-6 py-5 items-center">
+                    <div class="flex px-6 pt-5 items-center">
                         <input type="text" class="border border-gray-500 rounded h-8 placeholder:text-sm placeholder:text-gray-300 w-4/5" placeholder="TODOを入力">
 
                         <div class="ml-2 flex flex-col items-center cursor-pointer" id="detail">
-                            <p class="text-sm -mb-1 hover:underline">詳細</p>
-                            <p class="text-sm -mb-1 hover:underline hidden">閉じる</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                            <p class="text-sm -mb-1 hover:underline select-none" id="detail_sign">詳細</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4" id="down_arrow">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                             </svg>
                         </div>
-                        <button class="bg-[#8b8a8e] text-white text-sm px-4 py-2 rounded h-8 ml-auto hover:bg-opacity-80">追加</button>
+                        <button class="bg-[#8b8a8e] text-white text-sm px-4 py-2 rounded h-8 ml-auto hover:bg-opacity-80 select-none">追加</button>
                     </div>
-                    <div class="">
-                        <textarea placeholder="内容" class="placeholder:text-sm placeholder:text-gray-300"></textarea>
-                        <label for="percentage-dropdown">進捗率</label>
-                        <select id="percentage-dropdown">
-                            @for($i = 0; $i <= 100; $i+10)
-                            <option value="{{ $i }}">{{ $i }}</option>
-                            @endfor
-                            {{-- <option value="10">10%</option>
-                            <option value="20">20%</option>
-                            <option value="30">30%</option>
-                            <option value="40">40%</option>
-                            <option value="50">50%</option>
-                            <option value="60">60%</option>
-                            <option value="70">70%</option>
-                            <option value="80">80%</option>
-                            <option value="90">90%</option>
-                            <option value="100">100%</option> --}}
-                        </select>
-
+                    <div class="px-6 pb-5 pt-3 hidden" id="todo_content">
+                        <div>
+                            <textarea placeholder="内容" class="placeholder:text-sm placeholder:text-gray-300 w-4/5 rounded"></textarea>
+                        </div>
+                        <div class="flex flex-wrap pt-1">
+                            <div class="w-full sm:w-auto mb-4 sm:mb-0">
+                                <label for="percentage">進捗率</label>
+                                <select id="percentage" class="rounded mr-4">
+                                    <option value="">--</option>
+                                    @for($i = 0; $i <= 100; $i += 10)
+                                        <option value="{{ $i }}">{{ $i }}%</option>
+                                    @endfor
+                                </select>
+                            </div>
+                            <div class="w-full sm:w-auto mb-4 sm:mb-0">
+                                <label for="priority">優先度</label>
+                                <select id="priority" class="rounded mr-4">
+                                    <option value="">--</option>
+                                    <option value="high">高</option>
+                                    <option value="middle">中</option>
+                                    <option value="low">低</option>
+                                </select>
+                            </div>
+                            <div class="w-full sm:w-auto mb-4 sm:mb-0">
+                                <label for="due">期日</label>
+                                <input type="date" name="due" id="due" class="rounded">
+                            </div>
+                            {{--余裕あるならここにラベルを追加--}}
+                        </div>
                     </div>
                 </form>
+
+                <div class="border border-gray-100 my-4"></div>
 
                 <table class="w-full text-left table-auto">
                     <thead>
