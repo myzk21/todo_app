56d1fd0ee52d5c624c56075c8d4099459a2c49a5
Sota Miyazaki <luckypaper@icloud.com>
2025-02-10 01:18:44 +0900

    [Update] 送信時に最新のcsrfトークンを取得するように変更

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
     Route::get('/', [TodoController::class, 'index'])->name('home');
 
+    //最新のcsrfトークンを取得
+    Route::get('/csrf-token', function () {
+        return response()->json(['token' => csrf_token()]);
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
